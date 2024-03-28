5fd920a45764adf4dc7516d549222ed01ac44a3d
Justin Brown <108198176+JustinBrown17@users.noreply.github.com>
2024-03-27 21:47:15 -0400

    remove old code

--- Amortization Calculator/functions.py
+++ Amortization Calculator/functions.py
@@ -47,6 +47,9 @@ def prompt_user():
     Returns:
         dict: A dictionary containing all the user input data.
     """
+    # TODO: add frequency into user_data
+    # TODO: add more payments??
+
     # Initialize an empty dictionary to store user data
     user_data = {}
 
@@ -101,81 +104,7 @@ def prompt_user():
 
     return user_data
 
-'''def prompt_user():
-    """
-    This function prompts the user with all necessary data for calculation of the loan
 
-    Returns:
-    dict: With all the user data inputs
-    """
-    #user_data = {}
-
-    # Get loan info
-    principal = get_float_input("Enter the loan amount (principal): ")
-    interest_rate = get_float_input("Enter the annual interest rate (as a decimal): ")
-    loan_term_years = int(input("Enter the loan term (in years): "))
-    loan_start_date = get_date_input("Enter the start date of the loan (YYYY-MM-DD): ")
-    
-
-    # Get one time payments info
-    one_time_payment = get_float_input("Enter one-time extra payment (if applicable): ")
-
-    # Additional questions if user make one-time payment
-    if one_time_payment != 0:
-        one_time_payment_date = get_date_input("Enter the start date for one time payment (YYYY-MM-DD): ")
-        one_time_payment2 = get_float_input("Enter 2nd one-time extra payment (if applicable): ")
-        
-        # If valid 2nd one-time payment, ask for date
-        if one_time_payment2 != 0:
-            one_time_payment2_date = get_date_input("Enter the start date for 2nd one time payment (YYYY-MM-DD): ")
-    
-     # If user did not make a one time payment do not ask them the date or second one-time payment
-    else: 
-        one_time_payment_date = loan_start_date
-    
-
-    # Get extra payment info
-    extra_monthly_payment = get_float_input("Enter the recurring extra payment: ")
-    
-    
-    # TODO: add frequency into user_data
-    # TODO: add more payments??
-
-    if extra_monthly_payment != 0:
-        extra_monthly_payment_start_date = get_date_input("Enter the start date for extra payments (YYYY-MM-DD): ")
-    else: 
-        extra_monthly_payment_start_date = loan_start_date
-    
-
-    # Get additional payment info
-    addl_extra_payment = get_float_input("Enter the later date additional payment (if applicable): ")
-    
-    if addl_extra_payment != 0:
-        addl_extra_payment_start_date = get_date_input("Enter the start date for the additional extra payments (YYYY-MM-DD): ")
-    else:
-        addl_extra_payment_start_date = loan_start_date
-
-    # Get last payment amount
-    last_payment_amount = get_float_input("Enter the increased last payment amount (if desired): ")
-
-    # Collect user data into dict
-    user_data = {
-        "Principal" : principal,
-        "Interest Rate" : interest_rate,
-        "Loan Term Years" : loan_term_years,
-        "Loan Start Date" : loan_start_date,
-        "One Time Payment Amount" : one_time_payment,
-        "One Time Payement Date" : one_time_payment_date,
-        "Second One Time Payment Amount" : one_time_payment2,
-        "Second One Time Payment Date" : one_time_payment2_date,
-        "Extra Payment Amount" : extra_monthly_payment,
-        "Extra Payment Start Date" : extra_monthly_payment_start_date,
-        "Additional Extra Payment Amount" : addl_extra_payment,
-        "Additional Extra Payment Start Date" : addl_extra_payment_start_date,
-        "Last Payment Amount" : last_payment_amount, # Desired last payment
-    }
-
-    return user_data'''
 
 def auto_set_prompt():
     """
